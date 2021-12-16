683894ce0680710ce12eeaae4b816e742fc16e17
Aditya5singhal <77671226+Aditya5singhal@users.noreply.github.com>
2021-12-16 19:59:20 +0530

    Add files via upload

---
+++ pclass105/p105.py
@@ -0,0 +1,35 @@
+import csv
+import pandas as pd
+import plotly.express as px
+
+
+with open('data.csv',newline='')as f:
+    reader=csv.reader(f)
+    filedata=list(reader)
+#filedata.pop(0)
+data=filedata[0]
+total_marks = 0
+total_entries = len(data)
+print(total_entries)
+for marks in data:
+    total_marks += int(marks)
+print(total_marks)
+mean = total_marks / total_entries
+print("Mean (Average) is -> "+str(mean))
+
+
+
+    
+import math
+squarelist=[]
+for i in data:
+    a=int((i)-mean(data))
+    a=a**2
+    squarelist.append(a)
+    sum=0
+    for i in squarelist:
+        sum=sum+i
+
+result=sum/n   
+sd=math.sqrt(result) 
+print(sd)
